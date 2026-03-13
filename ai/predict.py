783e2895b032625f3955778940de3f6c07852feb
Fewdev1122 <phumiphatasunee478@gmail.com>
2026-03-13 21:19:18 +0700

    .

--- ai/predict.py
+++ ai/predict.py
@@ -1,70 +1,27 @@
-import tensorflow as tf
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 import numpy as np
 from PIL import Image
 from flask import Flask, request, jsonify
+import tensorflow as tf
 
-MODEL_PATH = "model/final_disaster_model.keras"
+MODEL_PATH = "model/disaster_model.tflite"
 IMG_SIZE = (224, 224)
 CLASS_NAMES = ['dust', 'fire', 'flood', 'fog', 'normal', 'smoke']
 
 CONFIDENCE_THRESHOLD = 60.0
-MARGIN_THRESHOLD = 15.0
-
-
-class WeightedCategoricalFocalLoss(tf.keras.losses.Loss):
-    def __init__(
-        self,
-        class_weights=None,
-        gamma=2.0,
-        from_logits=False,
-        reduction="sum_over_batch_size",
-        name="weighted_categorical_focal_loss"
-    ):
-        super().__init__(reduction=reduction, name=name)
-        self.gamma = gamma
-        self.from_logits = from_logits
-        self.class_weights = class_weights
-
-        self.class_weights_tensor = None
-        if class_weights is not None:
-            if isinstance(class_weights, dict):
-                weights = [class_weights[i] for i in range(len(class_weights))]
-            else:
-                weights = class_weights
-            self.class_weights_tensor = tf.constant(weights, dtype=tf.float32)
-
-    def call(self, y_true, y_pred):
-        y_true = tf.cast(y_true, tf.float32)
-
-        if self.from_logits:
-            y_pred = tf.nn.softmax(y_pred, axis=-1)
-
-        y_pred = tf.clip_by_value(y_pred, 1e-7, 1.0 - 1e-7)
-        ce = -y_true * tf.math.log(y_pred)
-        focal = tf.pow(1.0 - y_pred, self.gamma) * ce
-
-        if self.class_weights_tensor is not None:
-            focal = focal * self.class_weights_tensor
-
-        return tf.reduce_sum(focal, axis=-1)
-
-    def get_config(self):
-        config = super().get_config()
-        config.update({
-            "class_weights": self.class_weights,
-            "gamma": self.gamma,
-            "from_logits": self.from_logits,
-        })
-        return config
-
-
-model = tf.keras.models.load_model(
-    MODEL_PATH,
-    custom_objects={"WeightedCategoricalFocalLoss": WeightedCategoricalFocalLoss},
-    compile=False
-)
 
 app = Flask(__name__)
+app.config["MAX_CONTENT_LENGTH"] = 20 * 1024 * 1024
+
+print("Loading TFLite model...")
+interpreter = tf.lite.Interpreter(model_path=MODEL_PATH)
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+print("TFLite model loaded")
 
 
 def preprocess_image(image: Image.Image):
@@ -74,7 +31,6 @@ def preprocess_image(image: Image.Image):
     img_array = np.array(image, dtype=np.float32)
     img_array = tf.keras.applications.efficientnet.preprocess_input(img_array)
     img_array = np.expand_dims(img_array, axis=0)
-
     return img_array
 
 
@@ -97,7 +53,10 @@ def predict():
     image = Image.open(file.stream).convert("RGB")
 
     img_array = preprocess_image(image)
-    predictions = model.predict(img_array, verbose=0)[0]
+
+    interpreter.set_tensor(input_details[0]["index"], img_array)
+    interpreter.invoke()
+    predictions = interpreter.get_tensor(output_details[0]["index"])[0]
 
     sorted_indices = np.argsort(predictions)[::-1]
     top1_idx = int(sorted_indices[0])
@@ -135,4 +94,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
